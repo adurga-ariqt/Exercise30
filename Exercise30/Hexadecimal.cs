308f42c58962408ecec87d9012806a1ddf00b8c2
adurga-ariqt <adurga@ariqt.com>
2022-10-31 17:08:00 +0530

    Add project files.

---
+++ Exercise30/Hexadecimal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise30
+{
+    internal class Hexadecimal
+    {
+        public static void Main()
+        {
+
+            string hexval = "4c0";
+            Console.WriteLine("Hexadecimal number: " + hexval);
+            int decValue = int.Parse(hexval, System.Globalization.NumberStyles.HexNumber);
+            Console.WriteLine("Convert to-");
+            Console.WriteLine("Decimal number: " + decValue);
+        }
+    }
+}
